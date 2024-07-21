2987fc60f812408647151e3c92ba4e7ee03122c5
mathew-tran <mathew.tran.dev@gmail.com>
2024-07-20 21:13:12 -0700

    add shader for brick from godot shaders

--- Scripts/Brick.gd
+++ Scripts/Brick.gd
@@ -8,14 +8,30 @@ var bIsDead = false
 signal Killed
 
 @export var PointsGained = 20
+@export var ShineDuration : float = 1.0
+@export var ShineIntensity : float = 1.0
+
+@onready var HighlightMaterial = preload("res://Shader/Brick.tres")
 
 func _enter_tree():
 	$Sprite2D.scale = Vector2.ZERO
+var time_passed : float = 0.0
 
 func _ready():
-
+	time_passed = randf_range(0.0, 80.0)
 	var tween = get_tree().create_tween()
 	tween.tween_property($Sprite2D, "scale", Vector2(1,1), .3)
+
+
+
+
+func _process(delta: float) -> void:
+	time_passed += delta
+	if HighlightMaterial:
+		$Sprite2D.material.set_shader_parameter("shine_duration", ShineDuration)
+		$Sprite2D.material.set_shader_parameter("shine_intensity", ShineIntensity)
+		$Sprite2D.material.set_shader_parameter("time", time_passed)
+
 
 func _on_area_2d_body_entered(body):
 	if body is Ball:
